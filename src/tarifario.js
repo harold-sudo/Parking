b493d44c121c9fcc17919fa9e383f6740369fa55
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-08 22:54:59 -0400

    Refactoring: Simplicación de la función

--- src/tarifario.js
+++ src/tarifario.js
@@ -1,34 +1,48 @@
 function tarifario(hora, minutos, turno) {
-    if (turno === "am") {
-        if (minutos > 0) {
-            hora += 1; // redondear hacia arriba si hay minutos adicionales
-        }
-        if (hora === 1) {
-            return "BS. 10.00";
-        }else if (hora === 2) {
-            return "BS. 20.00";
-        }else if (hora === 3) {
-            return "BS. 30.00";
-        }else if (hora === 4) {
-            return "BS. 40.00";
-        }else if (hora >= 5) {
-            return "BS. 50.00";
-        }
+    if (minutos > 0) {
+        hora += 1; 
     }
-    else{
-        if (minutos > 0) {
-            hora += 1; // redondear hacia arriba si hay minutos adicionales
-        }
-        if (hora === 1) {
-            return "BS. 6.00";
-        }else if (hora === 2) {
-            return "BS. 12.00";
-        }else if (hora === 3) {
-            return "BS. 18.00";
-        }else if (hora === 4) {
-            return "BS. 24.00";
-        }
+
+    let tarifa = turno === "am" ? 10 : 6;
+    let tope = turno === "am" ? 50 : null;
+
+    let total = hora * tarifa;
+
+    if(tope !== null && total > tope) {
+        total = tope;
     }
+
+    return `BS. ${total.toFixed(2)}`;
 }
 
 export default tarifario;
+// if (turno === "am") {
+//     if (minutos > 0) {
+//         hora += 1; // redondear hacia arriba si hay minutos adicionales
+//     }
+//     if (hora === 1) {
+//         return "BS. 10.00";
+//     }else if (hora === 2) {
+//         return "BS. 20.00";
+//     }else if (hora === 3) {
+//         return "BS. 30.00";
+//     }else if (hora === 4) {
+//         return "BS. 40.00";
+//     }else if (hora >= 5) {
+//         return "BS. 50.00";
+//     }
+// }
+// else{
+//     if (minutos > 0) {
+//         hora += 1; // redondear hacia arriba si hay minutos adicionales
+//     }
+//     if (hora === 1) {
+//         return "BS. 6.00";
+//     }else if (hora === 2) {
+//         return "BS. 12.00";
+//     }else if (hora === 3) {
+//         return "BS. 18.00";
+//     }else if (hora === 4) {
+//         return "BS. 24.00";
+//     }
+// }
