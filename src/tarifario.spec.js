6cdf3dbeb48da02ec29307e5793c8dd359e6943d
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-08 20:05:02 -0400

    Test: Tarifar fraccionado

--- src/tarifario.spec.js
+++ src/tarifario.spec.js
@@ -1,11 +1,15 @@
 import tarifario from "./tarifario.js";
 
 describe("Tarifario", () => {
-  it("tarifar para 1 hora", () => {
-    expect(tarifario(1)).toEqual("10 bs.");
-  });
+    it("tarifar para 30 min (fraccionado)", () => {
+        expect(tarifario(0, 30)).toEqual("10 bs.");
+    });
 
-  it("tarifar para 1 hora 30 min (fraccionado)", () => {
-    expect(tarifario(1, 30)).toEqual("20 bs.");
-  });
+    it("tarifar para 1 hora", () => {
+        expect(tarifario(1)).toEqual("10 bs.");
+    });
+
+    it("tarifar para 1 hora 30 min (fraccionado)", () => {
+        expect(tarifario(1, 30)).toEqual("20 bs.");
+    });
 });
