f7714fb232b6876a546a06c62df5f2142112707e
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-08 22:46:22 -0400

    Feat: Tarifa nocturna

--- src/tarifario.js
+++ src/tarifario.js
@@ -1,17 +1,33 @@
-function tarifario(hora, minutos) {
-    if (minutos > 0) {
-        hora += 1; // redondear hacia arriba si hay minutos adicionales
+function tarifario(hora, minutos, turno) {
+    if (turno === "am") {
+        if (minutos > 0) {
+            hora += 1; // redondear hacia arriba si hay minutos adicionales
+        }
+        if (hora === 1) {
+            return "BS. 10.00";
+        }else if (hora === 2) {
+            return "BS. 20.00";
+        }else if (hora === 3) {
+            return "BS. 30.00";
+        }else if (hora === 4) {
+            return "BS. 40.00";
+        }else if (hora >= 5) {
+            return "BS. 50.00";
+        }
     }
-    if (hora === 1) {
-        return "BS. 10.00";
-    }else if (hora === 2) {
-        return "BS. 20.00";
-    }else if (hora === 3) {
-        return "BS. 30.00";
-    }else if (hora === 4) {
-        return "BS. 40.00";
-    }else if (hora >= 5) {
-        return "BS. 50.00";
+    else{
+        if (minutos > 0) {
+            hora += 1; // redondear hacia arriba si hay minutos adicionales
+        }
+        if (hora === 1) {
+            return "BS. 6.00";
+        }else if (hora === 2) {
+            return "BS. 12.00";
+        }else if (hora === 3) {
+            return "BS. 18.00";
+        }else if (hora === 4) {
+            return "BS. 24.00";
+        }
     }
 }
 
--- src/tarifario.spec.js
+++ src/tarifario.spec.js
@@ -2,19 +2,23 @@ import tarifario from "./tarifario.js";
 
 describe("Tarifario", () => {
     it("tarifar para 30 min (fraccionado)", () => {
-        expect(tarifario(0, 30)).toEqual("BS. 10.00");
+        expect(tarifario(0, 30, "am")).toEqual("BS. 10.00");
     });
 
     it("tarifar para 1 hora", () => {
-        expect(tarifario(1)).toEqual("BS. 10.00");
+        expect(tarifario(1, 0, "am")).toEqual("BS. 10.00");
     });
 
     it("tarifar para 1 hora 30 min (fraccionado)", () => {
-        expect(tarifario(1, 30)).toEqual("BS. 20.00");
+        expect(tarifario(1, 30, "am")).toEqual("BS. 20.00");
     });
 
     it("tarifar con tope máximo", () => {
-        expect(tarifario(12)).toEqual("BS. 50.00");
+        expect(tarifario(12, 0, "am")).toEqual("BS. 50.00");
+    });
+
+    it("tarifar de noche", () => {
+        expect(tarifario(4, 0,"pm")).toEqual("BS. 24.00");
     });
 
 });
