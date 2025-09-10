5364fdc1d24d4090c640f8e9a7937d29d3571bc1
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-10 18:43:53 -0400

    Test: Tarifa 80 en cualquier caso de perder ticket

--- src/tarifario.js
+++ src/tarifario.js
@@ -1,25 +1,28 @@
 
-function tarifario(hora, minutos, turno) {
+function tarifario(hora, minutos, turno, ticket) {
   let total = 0;
-  if (turno === "diurno") {
-      if (minutos > 0) {
-          hora += 1; // redondear hacia arriba si hay minutos adicionales
-      }
-      total = hora * 10;
-      if (total > 50) {
-          total = 50; // aplicar tope máximo
-      }
-  }
-  else{  
-    if (minutos > 0) {
-          hora += 1; // redondear hacia arriba si hay minutos adicionales
-      }
-    if (hora <= 8)
-    {
-      total = hora * 6;
+  if (ticket === "no") {
+    if (turno === "diurno") {
+        if (minutos > 0) {
+            hora += 1; // redondear hacia arriba si hay minutos adicionales
+        }
+        total = hora * 10;
+        if (total > 50) {
+            total = 50; // aplicar tope máximo
+        }
     }
-  }
-    
+    else{  
+      if (minutos > 0) {
+            hora += 1; // redondear hacia arriba si hay minutos adicionales
+        }
+      if (hora <= 8)
+      {
+        total = hora * 6;
+      }
+    }
+  } else {
+    total = 80;
+  } 
   return `BS. ${total.toFixed(2)}`;
 }
 
