c69b04799a07e7271e521a1d7a7d13121cc6d586
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-10 14:47:33 -0400

    Test: Turno nocturno

--- src/tiempo.spec.js
+++ src/tiempo.spec.js
@@ -12,4 +12,10 @@ describe("Tiempo de estadía: ", () => {
         const salida = new Date(2025, 10, 9, 11, 30);
         expect(tiempo(ingreso, salida)).toEqual({ horas: 1, minutos: 30, tipo: "diurno" });
     });
+
+    it("debería cruzar medianoche (nocturno)", () => {
+        const ingreso = new Date(2023, 0, 1, 23, 30);
+        const salida = new Date(2023, 0, 2, 0, 30);
+        expect(tiempo(ingreso, salida)).toEqual({ horas: 1, minutos: 0, tipo: "nocturno" });
+    });
 });
