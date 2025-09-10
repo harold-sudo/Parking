a20dbad2c64a1b0c877336be9913075479a42dbb
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-09-10 14:37:07 -0400

    Test: obtener tiempo para 1hr y 30min

--- src/tiempo.spec.js
+++ src/tiempo.spec.js
@@ -1,9 +1,15 @@
 import tiempo from "./tiempo.js";
 
 describe("Tiempo de estadía: ", () => {
-    it("devolver para 1 hora", () => {
+    it("devolver para 1 hora exacta (diurno)", () => {
         const ingreso = new Date(2025, 10, 9, 10, 0);
         const salida = new Date(2025, 10, 9, 11, 0);
         expect(tiempo(ingreso, salida)).toEqual({ horas: 1, minutos: 0, tipo: "diurno" });
+    });
+
+    it("devolver para 1 hora 30 min (diurno)", () => {
+        const ingreso = new Date(2025, 10, 9, 10, 0);
+        const salida = new Date(2025, 10, 9, 11, 30);
+        expect(tiempo(ingreso, salida)).toEqual({ horas: 1, minutos: 30, tipo: "diurno" });
     });
 });
